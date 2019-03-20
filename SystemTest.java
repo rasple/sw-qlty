a1e985656dc8283572fbb2857f94fb41cb097053
rasple <frankme97@gmail.com>
2019-03-20 19:03:24 +0100

    fixed errors

--- SystemTest.java
+++ SystemTest.java
@@ -3,26 +3,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SystemTest {
-    
+
     public static void main(String[] args) {
 
         List<Resistor> resistors = new ArrayList<Resistor>();
         Scanner sc = new Scanner(System.in);
+        
 
         do{
             System.out.println("Neue Komponente!\nBitte Ausfallwahrscheinlichkeit eingeben: ");
             Resistor res = new Resistor(sc.nextDouble());
             resistors.add(res);
             System.out.println("Weitere Komponenten anlegen? (y/n)");
-            } while(sc.nextLine().matches("y"));
-            
-
-
-        }
-
-        System1 system1 = new System1();
-        System2 system2 = new System2();
+        } while(sc.nextLine().matches("y"));
+        sc.close();
+        //System1 system1 = new System1();
+        //System2 system2 = new System2();
         //System3 system3 = new System3();
     }
-
 }
