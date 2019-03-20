fbd6d57649916dab276fa8c0e28b18626d34160f
rasple <frankme97@gmail.com>
2019-03-20 18:34:52 +0100

    Renamed system

--- main.java
+++ main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        boolean abort = false;
+        Resistor[] resistors;
+
+        while(!abort){
+            SystemInterface.out.println("Neue Komponente!\nBitte Ausfallwahrscheinlichkeit eingeben: ");
+            Scanner sc = new Scanner(SystemInterface.in);
+            Resistor res = new Resistor(sc.nextDouble());
+            
+
+
+        }
+
+        System1 system1 = new System1();
+        System2 system2 = new System2();
+        //System3 system3 = new System3();
     }
 
 }
